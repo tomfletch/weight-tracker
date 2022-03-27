5cf3f673d72ee040e184702521e128e30e501f1d
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2022-03-27 12:31:49 +0100

    Create BMI bar

--- src/pages/StatsPage/components/StatsBar/StatsBar.tsx
+++ src/pages/StatsPage/components/StatsBar/StatsBar.tsx
@@ -8,6 +8,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowDown, faLongArrowUp } from '@fortawesome/free-solid-svg-icons';
 import HeightContext from '../../../../context/HeightContext';
 
+const BMI_NORMAL_MIN = 18.5;
+const BMI_NORMAL_MAX = 25;
+const BMI_OVERWEIGHT_MAX = 30;
+
+function map(value: number, from_min: number, from_max: number, to_min: number, to_max: number): number {
+  const from_range = from_max - from_min;
+  const to_range = to_max - to_min;
+
+  return (value - from_min) / from_range * to_range + to_min;
+}
+
+function limit(value: number, min: number, max: number): number {
+  return Math.min(max, Math.max(min, value));
+}
+
 function interpolateWeight(weightRecords: WeightRecord[], date: Date): number | null {
   let nearestRecordBefore: WeightRecord | null = null;
   let nearestRecordAfter: WeightRecord | null = null;
@@ -104,7 +119,39 @@ function BMIWidget() {
     const weight = weightRecords[weightRecords.length - 1].weightKgs;
     const bmi = weight / (height*height);
 
-    content = bmi.toFixed(1);
+    const bar_min = BMI_NORMAL_MIN - 1;
+    const bar_max = BMI_OVERWEIGHT_MAX + 1;
+
+    const map_bar = (value: number) => map(value, bar_min, bar_max, 0, 100);
+
+    const position = limit(map_bar(bmi), 0, 100);
+    const normal_min = map_bar(BMI_NORMAL_MIN);
+    const normal_max = map_bar(BMI_NORMAL_MAX);
+    const overweight_max = map_bar(BMI_OVERWEIGHT_MAX);
+
+    const normal_mid = map_bar((BMI_NORMAL_MIN + BMI_NORMAL_MAX) / 2);
+    const overweight_mid = map_bar((BMI_NORMAL_MAX + BMI_OVERWEIGHT_MAX) / 2);
+
+    content = (
+      <div className={styles.bmiBar}>
+        <div className={styles.bmiContainer}>
+          <div className={styles.bmi} style={{left: `${position}%`}}>{bmi.toFixed(1)}</div>
+        </div>
+        <div className={styles.arrowContainer}>
+          <div className={styles.arrow} style={{left: `${position}%`}}></div>
+        </div>
+        <div className={styles.bar}>
+          <div className={styles.underweight} style={{width: `${normal_min}%`}}></div>
+          <div className={styles.normal} style={{width: `${normal_max - normal_min}%`}}></div>
+          <div className={styles.overweight} style={{width: `${overweight_max - normal_max}%`}}></div>
+          <div className={styles.obese} style={{width: `${100 - overweight_max}%`}}></div>
+        </div>
+        <div className={styles.labels}>
+          <div style={{left: `${normal_mid}%`}}>Healthy</div>
+          <div style={{left: `${overweight_mid}%`}}>Overweight</div>
+        </div>
+      </div>
+    );
   }
 
 
