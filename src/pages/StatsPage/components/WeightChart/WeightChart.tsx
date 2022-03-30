27e8f37630bf37209ce66376c8189a4ee0ea690d
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2022-03-30 12:13:41 +0100

    Refactor creact tooltip

--- src/pages/StatsPage/components/WeightChart/WeightChart.tsx
+++ src/pages/StatsPage/components/WeightChart/WeightChart.tsx
@@ -11,6 +11,7 @@ import {
 } from '../../../../utils/weights';
 import SettingsContext from '../../../../context/SettingsContext';
 import { formatDate, toISODate } from '../../../../utils/dates';
+import { createTooltip } from '../../../../utils/chartjs';
 
 function WeightChart() {
   const { weightRecords, weightTargetKgs, weightUnit } = useContext(WeightContext);
@@ -127,51 +128,7 @@ function WeightChart() {
             return '';
           },
         },
-        external({ chart }: { chart: Chart}) {
-          // Tooltip Element
-          let tooltipEl = document.getElementById('chartjsTooltip');
-
-          // Create element on first render
-          if (!tooltipEl) {
-            tooltipEl = document.createElement('div');
-            tooltipEl.id = 'chartjsTooltip';
-            document.body.appendChild(tooltipEl);
-          }
-
-          // Hide if no tooltip
-          const tooltipModel = chart.tooltip;
-          if (!tooltipModel || tooltipModel.opacity === 0) {
-            tooltipEl.style.opacity = '0';
-            return;
-          }
-
-          const date = new Date(chart.tooltip.dataPoints[0].parsed.x);
-
-          // Set Text
-          if (tooltipModel.body) {
-            const bodyLines = tooltipModel.body.map((bodyItem) => bodyItem.lines);
-
-            let innerHtml = `<div class="date">${formatDate(date)}</div>`;
-
-            bodyLines.forEach((body, i) => {
-              innerHtml += `<div>${body}</div>`;
-            });
-
-            tooltipEl.innerHTML = innerHtml;
-          }
-
-          const position = chart.canvas.getBoundingClientRect();
-          // const bodyFont = Chart.helpers.toFont(tooltipModel.options.bodyFont);
-
-          // Display, position, and set styles for font
-          tooltipEl.style.opacity = '1';
-          tooltipEl.style.position = 'absolute';
-          tooltipEl.style.left = `${position.left + window.pageXOffset + tooltipModel.caretX}px`;
-          tooltipEl.style.top = `${position.top + window.pageYOffset + tooltipModel.caretY}px`;
-          // tooltipEl.style.font = bodyFont.string;
-          // tooltipEl.style.padding = tooltipModel.padding + 'px ' + tooltipModel.padding + 'px';
-          tooltipEl.style.pointerEvents = 'none';
-        },
+        external: createTooltip,
       },
     },
     interaction: {
--- src/utils/chartjs.ts
+++ src/utils/chartjs.ts
@@ -1,4 +1,6 @@
+/* eslint-disable import/prefer-default-export */
 import { Chart, registerables } from 'chart.js';
+import { formatDate } from './dates';
 
 const showLabelPlugin = {
   id: 'showlabel',
@@ -37,3 +39,49 @@ const showLabelPlugin = {
 
 Chart.register(...registerables);
 Chart.register(showLabelPlugin);
+
+export function createTooltip({ chart }: { chart: Chart}) {
+  // Tooltip Element
+  let tooltipEl = document.getElementById('chartjsTooltip');
+
+  // Create element on first render
+  if (!tooltipEl) {
+    tooltipEl = document.createElement('div');
+    tooltipEl.id = 'chartjsTooltip';
+    document.body.appendChild(tooltipEl);
+  }
+
+  // Hide if no tooltip
+  const tooltipModel = chart.tooltip;
+  if (!tooltipModel || tooltipModel.opacity === 0) {
+    tooltipEl.style.opacity = '0';
+    return;
+  }
+
+  const date = new Date(chart.tooltip.dataPoints[0].parsed.x);
+
+  // Set Text
+  if (tooltipModel.body) {
+    const bodyLines = tooltipModel.body.map((bodyItem) => bodyItem.lines);
+
+    let innerHtml = `<div class="date">${formatDate(date)}</div>`;
+
+    bodyLines.forEach((body, i) => {
+      innerHtml += `<div>${body}</div>`;
+    });
+
+    tooltipEl.innerHTML = innerHtml;
+  }
+
+  const position = chart.canvas.getBoundingClientRect();
+  // const bodyFont = Chart.helpers.toFont(tooltipModel.options.bodyFont);
+
+  // Display, position, and set styles for font
+  tooltipEl.style.opacity = '1';
+  tooltipEl.style.position = 'absolute';
+  tooltipEl.style.left = `${position.left + window.pageXOffset + tooltipModel.caretX}px`;
+  tooltipEl.style.top = `${position.top + window.pageYOffset + tooltipModel.caretY}px`;
+  // tooltipEl.style.font = bodyFont.string;
+  // tooltipEl.style.padding = tooltipModel.padding + 'px ' + tooltipModel.padding + 'px';
+  tooltipEl.style.pointerEvents = 'none';
+}
