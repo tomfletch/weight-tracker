153fec50832ca9c9330a1a5c32c32111e5821919
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2026-04-20 20:01:23 +0100

    Remove migration from legacy localStorage to zustand

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
-import { runLegacyLocalStorageToAppStoreMigration } from './migrations/legacyLocalStorageToAppStore';
 
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
   throw new Error('Root element not found');
 }
-
-runLegacyLocalStorageToAppStoreMigration();
 
 createRoot(rootElement).render(
   <StrictMode>
